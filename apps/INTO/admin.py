e927157c7820a89e04b78f552942b36a349c83d6
Pablo <dc16009@ues.edu.sv>
2019-01-03 18:23:54 -0600

    Merge

--- apps/INTO/admin.py
+++ apps/INTO/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from apps.INTO.models import *
 # Register your models here.
+admin.site.register(Docente)
+admin.site.register(TipoUsuario)
+admin.site.register(asignacionTipoUsuario)
+admin.site.register(Especialidad)
+admin.site.register(Encargado)
+admin.site.register(Alumno)
+admin.site.register(Materia)
+admin.site.register(Especialidad_Materia)
+admin.site.register(Grupo)
+admin.site.register(Alumno_Grupo)
+admin.site.register(AnioLectivo)
+admin.site.register(Periodo)
+admin.site.register(Tipo_actividad)
+admin.site.register(Actividad)
+admin.site.register(Sub_Actividad)
+admin.site.register(Docente_Materia)
+admin.site.register(Evaluacion)
+admin.site.register(Calificacion)
+admin.site.register(Anotacion)
