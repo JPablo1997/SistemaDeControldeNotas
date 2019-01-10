17dab3b9c4ce55acfb6c96eca0349b4749ac5823
diegogarcia10 <ddjochoa.20@gmail.com>
2019-01-09 22:10:56 -0600

    Agregando mi parte en el SQL

--- SistemaDeControldeNotas/settings.py
+++ SistemaDeControldeNotas/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'SistemaDeControldeNotas',
     'USER':'postgres',
-    'PASSWORD':'dna',    
+    'PASSWORD':'administrador',    
 	'HOST':'localhost',
 	'PORT':5432,
     }
